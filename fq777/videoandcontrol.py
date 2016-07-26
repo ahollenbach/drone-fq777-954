fbc5eefcad2645ff7688e934fc0ffbf10a970d01
Willian Galvani <williangalvani@gmail.com>
2016-07-26 20:18:33 -0300

    change debug settings, playback file

--- fq777/videoandcontrol.py
+++ fq777/videoandcontrol.py
@@ -2,10 +2,16 @@ from dronecontrol import DroneControl
 from dronevideo import DroneVideo
 import time
 import os
+#from cStringIO import StringIO
+#from StringIO import StringIO
+from io import BytesIO as StringIO
 
 from opencvtest import decode
 
-RUNTIME = 180
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+RUNTIME = 10
 video = DroneVideo()
 drone = DroneControl()
 drone.connect()
@@ -13,29 +19,31 @@ start = time.time()
 stopTime = start+RUNTIME
 
 lasttime = start
+
+data = StringIO()
+
+
 while( time.time() < stopTime):
     try:
         drone.cmd(t=20)
 
         newdata = video.fetch_video()
         if newdata is not None:
-            with open("last.mp4", 'ab') as f:
-                f.write(newdata)
-
-        frames = decode("last.mp4")
-        if frames > 3:
-            videobuffer = ""
-            os.remove('last.mp4')
-
-
+            data.write(newdata)
         print( time.time() - lasttime, time.time() - start)
         lasttime = time.time()
-    except:
+    except Exception as e:
+        print(e)
         print("fuck! reconnecting!")
         video.reconnect()
         drone.reconnect()
 
 
+#decode(data)
+with open("stream.mp4",'wb') as f:
+    f.write(data.getvalue())
+
+decode("stream.mp4")
 print("run for ", time.time() - start)
 
 drone.stop()
